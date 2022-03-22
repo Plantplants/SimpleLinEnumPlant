34088647c98cea8bc17b02aa0c9b0a3442f47532
Plant Plants <cactusisdelicious@gmail.com>
2022-03-22 16:21:11 -0400

    Updated README.md and enumPlant.sh

--- enumPlant.sh
+++ enumPlant.sh
@@ -19,11 +19,41 @@ echo "The script will write to $output"
 # Enumeration process and writing to a file.
 {
 echo "Simple Linux Enumeration Plant Output"
-printRed "Kernel Version" # cat /proc/version
+printRed "Kernel Version"
 cat /proc/version
 
-printRed "Users with Home Directories" # grep -v "nologin\|false" /etc/passwd
+printRed "Current User Information"
+id
+
+printRed "Sudo Version"
+sudo -V
+
+printRed "Writable /etc/passwd?"
+ls -la /etc/passwd
+
+printRed "Users with Home Directories"
 grep -v "nologin\|false" /etc/passwd
+
+printRed "Cronjobs /etc/crontab"
+cat /etc/crontab
+
+printRed "Listening Ports"
+netstat -antup | grep "LISTEN"
+
+printRed "SUID"
+find / -perm /4000 -type f -exec ls -ld {} \; 2>/dev/null
+
+printRed "Capabilities"
+getcap -r / 2>/dev/null
+
+printRed "No Root Squash"
+cat /etc/exports | grep no_root_squash
+
+printRed "World Writable folders"
+find / -perm -o+w -type d -exec ls -la {} + 2>/dev/null
+
+printRed "World Writable Files"
+find / -perm -o+w -type f -exec ls -la {} + 2>/dev/null
 
 } > $output
 
